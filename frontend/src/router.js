a52cf2db2d36303375f875e0f18cae1e584c1aee
hugohebbinckuys <hugo29.heb@gmail.com>
2025-05-27 15:57:40 +0200

    ajout register & login views

---
+++ frontend/src/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+import login from "./views/login.vue";
+import register from "./views/register.vue";
+
+const routes = [
+    { path: '/login', name: 'Login', component: login}, 
+    { path: '/register', name: 'Register', component: register}
+]; 
+
+const router = createRouter({
+    history: createWebHistory(), 
+    routes,
+}); 
+
+export default router; 
