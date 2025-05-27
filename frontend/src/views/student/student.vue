3581e0b0318809125c7065cc950db9ead82ab2dc
hugohebbinckuys <hugo29.heb@gmail.com>
2025-05-27 22:30:57 +0200

    modifs on student page

--- frontend/src/views/student/student.vue
+++ frontend/src/views/student/student.vue
@@ -1,11 +1,19 @@
 <script setup>
 
-let count = 0
+// let count = 0
+let count = 5
+
+// function qui au clique ajoute a une liste les elements un  par un pour concserver le rang 
+// const addToList = () => {
+
+// }
 
 </script>
 
 <template>
     <p> / student / </p>
-
     <p v-if="count==0"> Pas de Form acutellement </p>
+    <form v-else v-for="elt in count">
+        <input type="checkbox" onclick="addToLit(elt)"> etudiant n°{{ elt }}
+    </form>
 </template>
