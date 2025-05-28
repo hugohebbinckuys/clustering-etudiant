83f229b505141e7a70dbcdae2bb8e97739fc1c0f
hugohebbinckuys <hugo29.heb@gmail.com>
2025-05-28 09:42:06 +0200

    modifs on student pages

--- frontend/src/views/student/student.vue
+++ frontend/src/views/student/student.vue
@@ -1,19 +1,37 @@
 <script setup>
+import { ref } from 'vue';
 
 // let count = 0
-let count = 5
+const student_total = ref(5)
+const nb_points = ref(100) 
 
 // function qui au clique ajoute a une liste les elements un  par un pour concserver le rang 
 // const addToList = () => {
 
 // }
 
+// const pointManager = (number) => {
+//     nb_points.value -= number
+// }
+const incrementer = () => {
+    nb_points.value ++; 
+}
+const decrementer = () => {
+    nb_points.value --; 
+}
+
 </script>
 
 <template>
     <p> / student / </p>
-    <p v-if="count==0"> Pas de Form acutellement </p>
-    <form v-else v-for="elt in count">
-        <input type="checkbox" onclick="addToLit(elt)"> etudiant n°{{ elt }}
-    </form>
+
+    <p v-if="student_total==0"> Pas de Form acutellement </p>
+    <div v-if="student_total>0">
+        <p> number to allocate : {{ nb_points }}</p>
+        <form v-for="elt in student_total">
+            <input type="text"> etudiant n°{{ elt }}
+            <input type="number" v-model="number" placeholder="points pour l'étudiant"> <span> <button :disabled="nb_points<=0" @click="decrementer()" value="number"> - </button> <button :disabled="nb_points>=100" @click="incrementer()" value="number"> + </button></span>
+        </form>
+    </div>
+
 </template>
