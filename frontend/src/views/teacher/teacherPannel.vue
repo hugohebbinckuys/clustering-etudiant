33b0b133f3120a9716b7bba3b6220852156d1413
hugohebbinckuys <hugo29.heb@gmail.com>
2025-05-28 11:42:08 +0200

    teacher modifs

---
+++ frontend/src/views/teacher/teacherPannel.vue
@@ -0,0 +1,18 @@
+<script setup> 
+
+let FormList = [1, 2, 3]
+
+
+</script>
+
+<template>
+<p v-if="FormList.length > 0">
+    voici la liste de vos forms : 
+</p>
+<ul>
+    <li v-for="elt in FormList">
+        {{ elt }} 
+    </li>
+</ul>
+
+</template>
